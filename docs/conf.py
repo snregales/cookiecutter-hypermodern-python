fff87f57475106b832c244e3e5de5747bf750917
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-10-10 21:14:18 +0200

    Configure Sphinx to use furo theme

--- docs/conf.py
+++ docs/conf.py
@@ -8,17 +8,5 @@ copyright = f"{datetime.now().year}, {author}"
 extensions = ["sphinx.ext.intersphinx"]
 intersphinx_mapping = {"mypy": ("https://mypy.readthedocs.io/en/stable/", None)}
 language = "en"
-html_static_path = ["_static"]
-html_theme = "alabaster"
-html_theme_options = {
-    "github_banner": "true",
-    "github_button": "true",
-    "github_count": "true",
-    "github_user": "cjolowicz",
-    "github_repo": "cookiecutter-hypermodern-python",
-    "github_type": "star",
-    "logo": "logo.png",
-    "logo_name": "true",
-    "fixed_sidebar": "true",
-    "sidebar_width": "250px",
-}
+html_theme = "furo"
+html_logo = "_static/logo.png"
